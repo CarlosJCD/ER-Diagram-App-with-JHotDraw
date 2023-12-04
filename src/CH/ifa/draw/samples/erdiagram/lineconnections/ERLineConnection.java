49fb97f22bd02a8f85fdce5841d512b428ff0d41
CarlosJCD <carlos.calderon.d@icloud.com>
2023-12-04 07:52:56 -0600

    Improved Connection Naming

--- src/CH/ifa/draw/samples/erdiagram/lineconnections/ERLineConnection.java
+++ src/CH/ifa/draw/samples/erdiagram/lineconnections/ERLineConnection.java
@@ -4,8 +4,8 @@ import CH.ifa.draw.figures.LineConnection;
 import CH.ifa.draw.figures.LineDecoration;
 
 public class ERLineConnection extends LineConnection {
-    public ERLineConnection(LineDecoration decorationTip1, LineDecoration decorationTip2) {
-        setStartDecoration(decorationTip1);
-        setEndDecoration(decorationTip2);
+    public ERLineConnection(LineDecoration startDecoration, LineDecoration endDecoration) {
+        setStartDecoration(startDecoration);
+        setEndDecoration(endDecoration);
     }
 }
